e53468a193df1c87f81cb78d3c3d0e4b3c1fddcd
Evgeniy-Bachilo <ebachilo@elinext.com>
2024-06-02 14:43:26 +0300

    feat: created base controller, express init

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -4,15 +4,19 @@ import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import express, { Express } from 'express';
 
 @injectable()
 export class RestApplication {
+  private readonly server: Express;
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient)
     private readonly databaseClient: DatabaseClient,
-  ) {}
+  ) {
+    this.server = express();
+  }
 
   private async initDb() {
     const mongoUri = getMongoURI(
@@ -26,11 +30,21 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
     this.logger.info('Init database…');
     await this.initDb();
     this.logger.info('Init database completed');
+    this.logger.info('Try to init server…');
+    await this.initServer();
+    this.logger.info(
+      `🚀 Server started on http://localhost:${this.config.get('PORT')}`,
+    );
   }
 }
---
+++ src/shared/libs/rest/index.ts
@@ -0,0 +1,3 @@
+export { HttpMethod } from './types/http-method.enum.js';
+export { Route } from './types/route.interface.js';
+export { Controller } from './controller/controller.interface.js';
